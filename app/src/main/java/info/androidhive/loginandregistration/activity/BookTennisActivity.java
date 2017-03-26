bf46f738504fb74bdb839c6826b86bf904494106
Sharavi95 <sharavi.ravi@live.com>
2017-03-26 16:08:24 +0400

    working tennis booking 1 page

--- app/src/main/java/info/androidhive/loginandregistration/activity/BookTennisActivity.java
+++ app/src/main/java/info/androidhive/loginandregistration/activity/BookTennisActivity.java
@@ -1,117 +1,115 @@
 package info.androidhive.loginandregistration.activity;
 
 import android.app.DatePickerDialog;
-import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 
 import info.androidhive.loginandregistration.R;
 
-public class BookTennisActivity extends AppCompatActivity {
+public class BookTennisActivity extends AppCompatActivity implements View.OnClickListener {
 
-    public String level;
-    private EditText mDateDisplay;
-    private ImageButton mPickDate;
-    private int mYear;
-    private int mMonth;
-    private int mDay;
-
-    static final int DATE_DIALOG_ID = 0;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_book_tennis);
-
-        mDateDisplay = (EditText) findViewById(R.id.date);
-        mPickDate = (ImageButton) findViewById(R.id.submitLevel);
-
-        mPickDate.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                showDialog(DATE_DIALOG_ID);
-            }
-        });
-
-        final Calendar c = Calendar.getInstance();
-        mYear = c.get(Calendar.YEAR);
-        mMonth = c.get(Calendar.MONTH);
-        mDay = c.get(Calendar.DAY_OF_MONTH);
-
-        updateDisplay();
-    }
-
-    private void updateDisplay() {
-        mDateDisplay.setText(
-                new StringBuilder()
-                        .append(mMonth + 1).append("-")
-                        .append(mDay).append("-")
-                        .append(mYear).append(" "));
-    }
-
-    private DatePickerDialog.OnDateSetListener mDateSetListener =
-            new DatePickerDialog.OnDateSetListener() {
-
-                public void onDateSet(DatePicker view, int year,
-                                      int monthOfYear, int dayOfMonth) {
-                    mYear = year;
-                    mMonth = monthOfYear;
-                    mDay = dayOfMonth;
-                    updateDisplay();
-                }
-            };
+    //UI References
+    private EditText sessionDate;
+    private Button submitLevel;
+    private DatePickerDialog datePickerDialog;
+    private SimpleDateFormat dateFormatter;
+    private RadioGroup playerLevel;
+    private RadioButton radioButton;
 
     @Override
-    protected Dialog onCreateDialog(int id) {
-        switch (id) {
-            case DATE_DIALOG_ID:
-                return new DatePickerDialog(this, mDateSetListener, mYear, mMonth,
-                        mDay);
-        }
-        return null;
-    }
-
-}
-/*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_tennis);
 
- /*       //****DATE SELECTION
-        // initiate the date picker and a button
-        date = (EditText) findViewById(R.id.date);
-        // perform click event on edit text
-        date.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // calender class's instance and get current date , month and year from calender
-                final Calendar c = Calendar.getInstance();
-                c.setFirstDayOfWeek(Calendar.SUNDAY);
-                int mYear = c.get(Calendar.YEAR); // current year
-                int mMonth = c.get(Calendar.MONTH); // current month
-                int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
-                // date picker dialog
-                DatePickerDialog datePickerDialog = new DatePickerDialog(BookTennisActivity.this,
-                        new DatePickerDialog.OnDateSetListener() {
+        dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
 
-                            @Override
-                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                                // set day of month , month and year value in the edit text
-                                date.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
-                            }
-                        }, mYear, mMonth, mDay);
-                datePickerDialog.show();
+        findViewsById();
+        setDateTimeField();
+        setPlayerLevel();
+
+    }
+
+    private void findViewsById() {
+        sessionDate = (EditText) findViewById(R.id.selectDate);
+        sessionDate.setInputType(InputType.TYPE_NULL);
+        submitLevel = (Button) findViewById(R.id.submitLevel);
+        playerLevel = (RadioGroup) findViewById(R.id.playerLevel);
+    }
+
+    private void setDateTimeField() {
+        sessionDate.setOnClickListener(this);
+
+        Calendar newCalendar = Calendar.getInstance();
+        datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                sessionDate.setText(dateFormatter.format(newDate.getTime()));
             }
 
+        }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+    }
 
-        });*/
+    private void setPlayerLevel() {
 
-        /*submitLevel.setOnClickListener(new View.OnClickListener() {
+        playerLevel = (RadioGroup) findViewById(R.id.playerLevel);
+        submitLevel = (Button) findViewById(R.id.submitLevel);
+
+        submitLevel.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                // get selected radio button from radioGroup
+                int selectedId = playerLevel.getCheckedRadioButtonId();
+
+                // find the radiobutton by returned id
+                radioButton = (RadioButton) findViewById(selectedId);
+                Toast.makeText(BookTennisActivity.this, radioButton.getText(), Toast.LENGTH_SHORT).show();
+            }
+
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == sessionDate) {
+            datePickerDialog.show();
+        }
+    }
+}
+
+/*public class BookTennisActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //UI References
+    private EditText sessionDate;
+    private DatePickerDialog datePickerDialog;
+    private SimpleDateFormat dateFormatter;
+    private String level;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_tennis);
+
+        dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
+
+        findViewsById();
+        setDateTimeField();
+        submitLevel.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 // Is the button now checked?
                 boolean checked = ((RadioButton) view).isChecked();
@@ -133,16 +131,38 @@ public class BookTennisActivity extends AppCompatActivity {
                             break;
                         }
                 }
+            }
+        });
+    }
 
+    private void findViewsById() {
+        sessionDate = (EditText) findViewById(R.id.selectDate);
+        sessionDate.setInputType(InputType.TYPE_NULL);
+    }
+
+    private void setDateTimeField() {
+        sessionDate.setOnClickListener(this);
+
+        Calendar newCalendar = Calendar.getInstance();
+        datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                sessionDate.setText(dateFormatter.format(newDate.getTime()));
             }
 
-        });
-        /*DATABASE BUILDING
-        DataBaseHelper myDbHelper = new DataBaseHelper(this);
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+    }
 
-        try { myDbHelper.createDataBase(); }
-        catch (IOException ioe) { throw new Error("Unable to create database"); }
+    @Override
+    public void onClick(View view) {
+        if(view == sessionDate) {
+            datePickerDialog.show();
+        }
+    }
 
-        try { myDbHelper.openDataBase(); }
-        catch (SQLException sqle) { throw sqle;}*/
+
+}
+*/
 
